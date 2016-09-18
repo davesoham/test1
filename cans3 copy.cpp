bc58a200a46637a03fda4fbe09bb584c1cb56ae5
davesoham <dave.soham1@gmail.com>
2016-09-18 12:29:15 -0700

    frst commit
    no unnecessary variable

--- cans3 copy.cpp
+++ cans3 copy.cpp
@@ -4,19 +4,6 @@ using namespace std;
 
 int main()
 {
-    int cans, cans1;
-    double test;
-    double x;
-    double y;
-
-    cout << "Enter no of gallons: ";
-    cin >> x;
-    cout << "\n" << x << " is number you entered.\n";
-
-    cout << "Enter capacity of one can: ";
-    cin >> y;
-    cout << "/n No u entered is: " << y << endl;
-
     cans = (int)x/y;
 
 
