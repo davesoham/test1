a8829857dc82dfe504e70ef7f3a4a0e961bea10f
davesoham <dave.soham1@gmail.com>
2016-09-18 12:27:28 -0700

    init
    version 1

---
+++ cans3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int cans, cans1;
+    double test;
+    double x;
+    double y;
+
+    cout << "Enter no of gallons: ";
+    cin >> x;
+    cout << "\n" << x << " is number you entered.\n";
+
+    cout << "Enter capacity of one can: ";
+    cin >> y;
+    cout << "/n No u entered is: " << y << endl;
+
+    cans = (int)x/y;
+
+
+    if ( fmod(x,y) != 0 )
+        cans++;
+    cout << cans << "\n";
+
+    return 0;
+
+}
